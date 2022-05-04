ec81974521c849efc0e9ed52f342759b1c57ffe9
Khélian Larvet <42818039+KhelianL@users.noreply.github.com>
2022-05-04 18:37:04 +0200

    ModeleComponent + starting Terrain

--- moteur/metier/GameObject/ModeleComponent.hpp
+++ moteur/metier/GameObject/ModeleComponent.hpp
@@ -11,29 +11,30 @@
 #include <Texture.hpp>
 #include <Collision/RigidBody.hpp>
 
-class ModeleComponent : public Component {
+class ModeleComponent : public Component
+{
 private:
-	GlobalShader* shader;
+	GlobalShader *shader;
+
 protected:
-	//Modele
+	// Modele
 	std::vector<glm::vec3> vertexArray;
 	std::vector<glm::vec2> texCoords;
 	std::vector<glm::vec3> normals;
 	std::vector<unsigned int> indices;
 
-	//Material
+	// Material
 	Material material;
 
-	//Texture
-	struct TextureContainer {
-		int id = -1;
-		Texture* texture = NULL;
+	// Texture
+	struct TextureContainer
+	{
+		Texture *texture = NULL;
 		bool destroyAtEnd;
 	};
-	int lastTextureID = 0;
-	TextureContainer texture;
+	std::vector<TextureContainer> textures;
 
-	//Buffers
+	// Buffers
 	GLuint VAO;
 	GLuint VBO[3];
 	GLuint EBO;
@@ -42,27 +43,30 @@ protected:
 	bool hasData;
 
 public:
-
 	// --- CONSTRUCTEURS ET DESTRUCTEURS ---
 
-	ModeleComponent(GlobalShader* shader, std::string fileOff) : Component(Component::Type::MODELE) {
+	ModeleComponent(GlobalShader *shader, std::string fileOff) : Component(Component::Type::MODELE)
+	{
 		this->shader = shader;
-		std::vector<unsigned short> indices; //Triangles concat�n�s dans une liste
-		std::vector<std::vector<unsigned short> > triangles;
+		std::vector<unsigned short> indices; // Triangles concat�n�s dans une liste
+		std::vector<std::vector<unsigned short>> triangles;
 
-		//Chargement du fichier de maillage
+		// Chargement du fichier de maillage
 		loadOFF(fileOff, this->vertexArray, indices, triangles);
-		for (int i = 0, max = indices.size(); i < max; i++) {
+		for (int i = 0, max = indices.size(); i < max; i++)
+		{
 			this->indices.push_back((int)indices[i]);
 		}
 		loadBuffer();
 	}
 
-	ModeleComponent(GlobalShader* shader) : Component(Component::Type::MODELE) {
+	ModeleComponent(GlobalShader *shader) : Component(Component::Type::MODELE)
+	{
 		this->shader = shader;
 	}
 
-	ModeleComponent(GlobalShader* shader, std::vector<glm::vec3> indexed_vertices, std::vector<glm::vec3> normals, std::vector<unsigned int> indices, std::vector<glm::vec2> texCoords) : Component(Component::Type::MODELE) {
+	ModeleComponent(GlobalShader *shader, std::vector<glm::vec3> indexed_vertices, std::vector<glm::vec3> normals, std::vector<unsigned int> indices, std::vector<glm::vec2> texCoords) : Component(Component::Type::MODELE)
+	{
 		this->shader = shader;
 		this->normals = normals;
 		this->vertexArray = indexed_vertices;
@@ -72,7 +76,8 @@ public:
 	}
 
 	//--- BUFFERS ---
-	virtual void loadBuffer() {
+	virtual void loadBuffer()
+	{
 		bool notEmptyTexture = (this->texCoords.size() > 0);
 		bool notEmptyNormal = (this->normals.size() > 0);
 
@@ -86,19 +91,21 @@ public:
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, this->VBO[0]);
 		glBufferData(GL_ARRAY_BUFFER, this->vertexArray.size() * sizeof(vec3), &this->vertexArray[0], GL_STATIC_DRAW);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
 
-		if (this->texCoords.size() > 0) {
+		if (this->texCoords.size() > 0)
+		{
 			glEnableVertexAttribArray(1);
 			glBindBuffer(GL_ARRAY_BUFFER, this->VBO[1]);
 			glBufferData(GL_ARRAY_BUFFER, this->texCoords.size() * sizeof(vec2), &this->texCoords[0], GL_STATIC_DRAW);
-			glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+			glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void *)0);
 		}
-		if (this->normals.size() > 0) {
+		if (this->normals.size() > 0)
+		{
 			glEnableVertexAttribArray(2);
 			glBindBuffer(GL_ARRAY_BUFFER, this->VBO[2]);
 			glBufferData(GL_ARRAY_BUFFER, this->normals.size() * sizeof(vec3), &this->normals[0], GL_STATIC_DRAW);
-			glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+			glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
 		}
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, this->indices.size() * sizeof(unsigned int), &this->indices[0], GL_STATIC_DRAW);
@@ -106,100 +113,125 @@ public:
 		this->hasData = true;
 	}
 
-
-
-	virtual void draw(Camera * camera, glm::mat4 transform, RigidBody * rigidbody = NULL) {
-		if (!hasData) {
+	virtual void draw(Camera *camera, glm::mat4 transform, RigidBody *rigidbody = NULL)
+	{
+		if (!hasData)
+		{
 			return;
 		}
 		bool isInFOV = false;
 
-		if(rigidbody !=NULL){
-			std::vector<glm::vec3> boxCoords =rigidbody->getCollision()->getBoundingBox()->getCoords();
-			for (int i = 0, max = boxCoords.size(); i < max && !isInFOV; i++) {
+		if (rigidbody != NULL)
+		{
+			std::vector<glm::vec3> boxCoords = rigidbody->getCollision()->getBoundingBox()->getCoords();
+			for (int i = 0, max = boxCoords.size(); i < max && !isInFOV; i++)
+			{
 				isInFOV = camera->isInFieldOfView(boxCoords[i]);
 			}
-		} else {
-			isInFOV = true; //TODO trouver une solution 
+		}
+		else
+		{
+			isInFOV = true; // TODO trouver une solution
 		}
 
-		if (!isInFOV) return;
+		if (!isInFOV)
+			return;
 
 		this->shader->use();
 
 		glEnable(GL_TEXTURE_2D);
 
-		this->shader->drawTexture(this->texture.texture, this->texture.id);
+		for (int i = 0, max = textures.size(); i < max; i++)
+		{
+			this->shader->drawTexture(this->textures[i].texture, i);
+		}
 
-		//LIGHT TEST
+		// LIGHT TEST
 		this->shader->setLight();
 		this->shader->drawMesh(this->VAO, this->indices.size(), transform, this->material);
 	}
 
-	void fillTextureData(TextureContainer* container, Texture* texture, bool destroyAtEnd) {
-		if (container->id == -1) {
-			container->id = lastTextureID++;
+	Texture *getTexture(int i)
+	{
+		return this->textures[i].texture;
+	}
+
+	std::vector<TextureContainer> getTexturesContainers()
+	{
+		return this->textures;
+	}
+
+	TextureContainer getTextureContainer(int i)
+	{
+		return this->textures[i];
+	}
+
+	void addTexture(Texture *texture, bool destroyAtEnd)
+	{
+		TextureContainer tc;
+		tc.texture = texture;
+		tc.destroyAtEnd = destroyAtEnd;
+		this->textures.push_back(tc);
+	}
+
+	void removeTexture(int i)
+	{
+		if (i > 0 && i < this->textures.size())
+		{
+			this->textures.erase(this->textures.begin() + i);
 		}
-		container->texture = texture;
-		container->destroyAtEnd = destroyAtEnd;
 	}
 
-	Texture* getTexture() {
-		return this->texture.texture;
-	}
-
-	TextureContainer getTextureContainer() {
-		return this->texture;
-	}
-
-	void setTexture(Texture* texture, bool destroyAtEnd) {
-		this->fillTextureData(&this->texture, texture, destroyAtEnd);
-	}
-	void removeTexture() {
-		this->texture.texture = NULL;
-	}
-
-	void setData(std::vector<glm::vec3> indexed_vertices, std::vector<unsigned int> indices, std::vector<glm::vec2> texCoords, std::vector<glm::vec3> normals) {
+	void setData(std::vector<glm::vec3> indexed_vertices, std::vector<unsigned int> indices, std::vector<glm::vec2> texCoords, std::vector<glm::vec3> normals)
+	{
 		this->vertexArray = indexed_vertices;
 		this->normals = normals;
 		this->indices = indices;
 		this->texCoords = texCoords;
 		loadBuffer();
 	}
-	std::vector<glm::vec3> getIndexedVertices() {
+	std::vector<glm::vec3> getIndexedVertices()
+	{
 		return this->vertexArray;
 	}
 
-	std::vector<glm::vec3> getNormals() {
+	std::vector<glm::vec3> getNormals()
+	{
 		return this->normals;
 	}
-	GlobalShader* getShader() {
+	GlobalShader *getShader()
+	{
 		return this->shader;
 	}
-	std::vector<unsigned int> getIndices() {
+	std::vector<unsigned int> getIndices()
+	{
 		return this->indices;
 	}
 
-	std::vector<glm::vec2> getTexCoords() {
+	std::vector<glm::vec2> getTexCoords()
+	{
 		return this->texCoords;
 	}
 
-	Material* getMaterial() {
+	Material *getMaterial()
+	{
 		return &this->material;
 	}
 
-	Material getStaticMaterial() {
+	Material getStaticMaterial()
+	{
 		return this->material;
 	}
 
-	void setMaterial(Material material) {
+	void setMaterial(Material material)
+	{
 		this->material = material;
 	}
 
-	GLuint getVAO() {
+	GLuint getVAO()
+	{
 		return this->VAO;
 	}
-
 };
 
 #endif
--- moteur/metier/GameObject/Terrain.hpp
+++ moteur/metier/GameObject/Terrain.hpp
@@ -2,114 +2,48 @@
 #define __H_TERRAIN__
 
 #include <glm/glm.hpp>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string>
 #include <vector>
+#include <iostream>
+#include <math.h>
 
 #include <common/shader.hpp>
 #include <common/objloader.hpp>
-#include <common/vboindexer.hpp>
 
+#include <Light/Material.hpp>
+#include <Texture.hpp>
+#include <Collision/Collision.hpp>
+#include <Shader/GlobalShader.hpp>
+#include <Tools/SimplifyMesh.hpp>
 
-#include "Modele.hpp"
-#include "../Shader/TerrainShader.hpp"
+#include "GameObject.hpp"
+#include <GameObject/ModeleComponent.hpp>
 
-class Terrain : public Modele {
-private:
-	TerrainShader* shader;
+class Terrain : public GameObject
+{
 protected:
-	//HeightMap
-	TextureContainer heightMap;
-	TextureContainer textureTop;
-	TextureContainer textureBottom;
+	ModeleComponent *modele;
+	Texture *texture;
+	// RIGIDBODY (collision)
 
-	float limitTop;
-	float limitBottom;
-	float limitBlur = 0.1;
+	/*
+	private:
+		GlobalShader* shader;
+		Material material;
+	*/
 
-public :
-	Terrain(std::string id, TerrainShader * shader, std::string fileOff, GameObject* parent = NULL) : Modele(id, shader, fileOff, parent) {
-		this->shader = shader;
-	}
-	Terrain(std::string id, TerrainShader* shader, GameObject* parent = NULL) : Modele(id, shader, parent) {
-		this->shader = shader;
-	}
-	Terrain(std::string id, std::vector<glm::vec3> indexed_vertices, std::vector<unsigned int> indices, std::vector<glm::vec2> texCoords, TerrainShader* shader, GameObject* parent = NULL)
-		: Modele(id, indexed_vertices, indices, texCoords,  shader, parent) 
+public:
+	Terrain()
 	{
-		this->shader = shader;
-	}
-
-
-
-
-	virtual ~Terrain() {
-		if (heightMap.texture != NULL && heightMap.destroyAtEnd) {
-			free(heightMap.texture);
-		}
-		if (textureTop.texture != NULL && textureTop.destroyAtEnd) {
-			free(textureTop.texture);
-		}
-		if (textureBottom.texture != NULL && textureBottom.destroyAtEnd) {
-			free(textureBottom.texture);
-		}
-	}
-
-	virtual void loadBuffer() {
-		Modele::loadBuffer();
-	}
-
-	void setHeightMap(Texture* tex, bool destroyAtEnd = false) {
-		this->fillTextureData(&this->heightMap, tex, destroyAtEnd);
-	}
-
-	void setTextureTop(Texture* tex, float limitTop = 0.7f, bool destroyAtEnd = false) {
-		this->limitTop = limitTop;
-		this->fillTextureData(&this->textureTop, tex, destroyAtEnd);
-	}
-
-	void setTextureBottom(Texture* tex, float limitBottom = 0.3f, bool destroyAtEnd = false) {
-		this->limitBottom = limitBottom;
-		this->fillTextureData(&this->textureBottom, tex, destroyAtEnd);
-	}
-
-
-	void setLimitTop(float limit) {
-		this->limitTop = limit;
-	}
-	void setLimitBottom(float limit) {
-		this->limitBottom = limit;
-	}
-	void setLimitBlur(float limit) {
-		this->limitBlur = limit;
-	}
-
-	virtual void compute(Camera* camera, bool dfs = true) {
-
-		draw();
-		if (dfs)
-			GameObject::compute(camera, dfs);
-	}
-
-	void draw(Camera* camera, bool dfs = true) {
-		if (!this->hasData) {
-			return;
-		}
-		this->shader->use();
-
-		glEnable(GL_TEXTURE_2D);
-
-		//Draw terrain information
-		this->shader->drawTextureTop(this->textureTop.texture, this->textureTop.id);
-		this->shader->drawTexture(this->texture.texture, this->texture.id);
-		this->shader->drawTextureBottom(this->textureBottom.texture, this->textureBottom.id);
-
-		this->shader->drawHeightMap(this->heightMap.texture, this->heightMap.id);
 		
-		this->shader->setLimit(this->limitTop, this->limitBottom, this->limitBlur);
-		//Draw Modele information
-		this->shader->drawMesh(this->VAO, this->indices.size(), this->getTransformMatrix());
+	}
+	~Terrain()
+	{
 
 	}
+	
 };
 
 #endif
